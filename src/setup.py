364f98feb2954f8b26591c1e4693b91327ddb9b0
Tim Rehbronn <tim.rehbronn@rwth-aachen.de>
2024-08-20 17:38:14 +0200

    created ROS package

--- src/setup.py
+++ src/setup.py
@@ -1,22 +1,18 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'calibration_transformation_broker'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    packages=[package_name],
+    package_dir={'': '.'},
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='timrehbronn',
-    maintainer_email='tim.rehbronn@rwth-aachen.de',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Dein Name',
+    maintainer_email='deine.email@example.com',
+    description='ROS2 node to read and extract transformations from a log file and broadcast them as tf2 transformations',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
